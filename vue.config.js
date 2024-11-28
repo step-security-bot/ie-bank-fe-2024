34166d5cda8d1f72433b6faffaab78560021bd2e
Shahaf Brenner <126162494+shahafbr@users.noreply.github.com>
2024-11-28 22:53:24 +0100

    Update vue.config.js
    try to fix the dev and uat issues

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
   transpileDependencies: true,
   configureWebpack: {
     devtool: "source-map"
-  }
-})
+  },
+  outputDir: process.env.NODE_ENV === 'development' 
+    ? 'dist-dev' // Output directory for development builds
+    : process.env.NODE_ENV === 'uat'
+    ? 'dist-uat' // Output directory for UAT builds
+    : 'dist', // Default output directory for production builds
+});
